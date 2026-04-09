eb1213a8b627fd9af5946cf030c7d6f49af16b86
Elnurkhan Geriev <egeriev@astralinux.ru>
2026-04-09 14:59:48 +0300

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import compression from 'vite-plugin-compression'
 
 export default defineConfig({
+  base: '/squareplans/',
   plugins: [
     vue(),
     // Gzip precompression
